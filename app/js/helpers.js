68f6daea3f83ca0c42692d3a5d4bb7ddc175cd21
DaJuan Harris <dajuancancode@gmail.com>
2019-11-23 00:22:33 -0500

    Created header and styled it for mobile breakpoint

--- app/js/helpers.js
+++ app/js/helpers.js
@@ -1,1 +1,9 @@
-console.log('World')
+const mobileMenu = document.querySelector('.mobile');
+const mobileIcon = document.querySelector('i');
+const navigationContainer = document.querySelector('.navigation-container')
+
+mobileMenu.addEventListener('click', () => {
+  navigationContainer.classList.toggle('show')
+  mobileIcon.classList.toggle("fa-times")
+  mobileIcon.classList.toggle("fa-bars")
+})
